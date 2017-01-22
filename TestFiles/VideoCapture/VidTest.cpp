9b5ba139eaeabbf2c18ddd2c67165c73c14bb058
msuinn <msuinn@ucsc.edu>
2017-01-21 18:53:13 -0800

    Video should be working now

--- TestFiles/VideoCapture/VidTest.cpp
+++ TestFiles/VideoCapture/VidTest.cpp
@@ -2,8 +2,29 @@
 #include <iostream>
 
 using namespace cv;
+using namespace std;
 
 int main(int, char**){
-	
 	cout << "Video?" << endl;
+	
+	VideoCapture capture(0);
+
+	if(!capture.isOpened()){
+		cout << "Camera not opened" << endl;
+	}
+
+	namedWindow("Video",1);
+	while(1){
+		Mat frame;
+		capture >> frame;
+		
+		imshow("Video", frame);
+		
+		if(waitKey(30) == 'c'){
+			break;
+		}
+	}
+
+	return(0);
+
 }
