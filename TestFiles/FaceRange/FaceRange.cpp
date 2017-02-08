5311b48863a73bc5aab7428b6cc9c5c740e4850b
msuinn <msuinn@ucsc.edu>
2017-02-07 16:09:46 -0800

    Working face range with arrow

--- TestFiles/FaceRange/FaceRange.cpp
+++ TestFiles/FaceRange/FaceRange.cpp
@@ -11,9 +11,6 @@
 using namespace cv;
 using namespace std;
 
- /** Function Headers */
- void detectAndDisplay( Mat color );
- void detectAndDisplay2( Mat ir );
 
  /** Global variables */
  String face_cascade_name = "haarcascade_frontalface_alt.xml";
@@ -22,7 +19,7 @@ using namespace std;
  CascadeClassifier eyes_cascade;
  string window_name = "Capture - Face detection";
  RNG rng(12345);
- Mat dpt;
+ //Mat dpt;
 
 int main(int, char**) try{
 
@@ -30,13 +27,14 @@ int main(int, char**) try{
    	if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
    	if( !eyes_cascade.load( eyes_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };	
 
-	//OpenCV Mat initialization
+	//Variables
 	const int matSize[2]={480,640};
+	const Point center(matSize[1]/2, matSize[0]/2);
+	vector<Rect> faces;
+	Mat frame_gray;
 
 	//Create the windows
-	namedWindow("Depth", 1);
 	namedWindow("Color", 1);
-	namedWindow("IR", 1);	
 
 	// Create a context object. This object owns the handles to all connected realsense devices.
     	rs::context ctx;
@@ -66,11 +64,11 @@ int main(int, char**) try{
 		//Depth
 		//Retrieve the depth frame from camera and place in new Mat
 		Mat depth(2, matSize, CV_16U, (uchar *) dev->get_frame_data(rs::stream::depth));
-		dpt = depth;		
+		Mat dpt(depth);		
 		//Compress down to uchar with one channel only for depth
 		depth.convertTo(depth, CV_8UC1, 255.0/1000);
 		//Apply color map depending on the depth value
-		applyColorMap(depth, depth, COLORMAP_RAINBOW);
+		//applyColorMap(depth, depth, COLORMAP_RAINBOW);
 		
 		//Color
 		//Retrieve the color frame from camera and place in new Mat
@@ -78,27 +76,35 @@ int main(int, char**) try{
 		//Convert color encoding so picutre makes sense
 		cvtColor(color, color, COLOR_BGR2RGB);
 
-		//Infrared
-		//Retrieve the infrared frame from camera and place in new Mat
-		Mat ir(2, matSize, CV_8U, (uchar *) dev->get_frame_data(rs::stream::infrared));
 
-		//-- 3. Apply the classifier to the frame
-       		if( !color.empty() )
-       		{ detectAndDisplay( color ); }
-       		else
-       		{ printf(" --(!) No captured frame -- Break!"); break; }
 
-		if( !ir.empty() )
-       		{ detectAndDisplay2( ir ); }
-       		else
-       		{ printf(" --(!) No captured frame -- Break!"); break; }
+		//Convert to gray for face detection function
+		cvtColor( color, frame_gray, CV_BGR2GRAY );
+  		equalizeHist( frame_gray, frame_gray );
+
+
+		//Detect faces and place coordinates in faces vector
+		face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 
+			0|CV_HAAR_SCALE_IMAGE, Size(30, 30) );
+
+
+		//For each face, find the center and draw an arrow from center to face
+		//Also print out the matching pixel depth from depth stream
+		for( size_t i = 0; i < faces.size(); i++ ){
+    			Point faceCenter( faces[i].x + faces[i].width*0.5, 
+				faces[i].y + faces[i].height*0.5 );
+			arrowedLine(color, center, faceCenter, Scalar( 94, 206, 165), 5);
+			cout << "Center " << faceCenter << endl;
+
+			cout << "Depth " << 
+				dpt.at<unsigned int16_t>(faceCenter.y, faceCenter.x)
+				<< endl;
+		}
 
 		
 		
-		//Show both of the views
-		imshow("Depth", depth);
+		//Show image with arrow
 		imshow("Color", color);
-		imshow("IR", ir);
 		
 		if(waitKey(30) == 'c'){
 			break;
@@ -115,67 +121,6 @@ catch(const rs::error & e)
     	return EXIT_FAILURE;
 }
 
-/** @function detectAndDisplay */
-void detectAndDisplay( Mat color )
-{
-  	std::vector<Rect> faces;
-  	Mat frame_gray;
 
-  	cvtColor( color, frame_gray, CV_BGR2GRAY );
-  	equalizeHist( frame_gray, frame_gray );
-
-  	//-- Detect faces
-  	face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30) );
-
-  	for( size_t i = 0; i < faces.size(); i++ )
-  	{
-    		Point center( faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5 );
-    		ellipse( color, center, Size( faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
-
-		int loc = 640 * (int)faces[i].y + (int)faces[i].x;		
-		printf("FACE %d x: %d y:%d range: %d\n", i, (int)faces[i].x, (int)faces[i].y, dpt.at<int>((int)faces[i].x,(int)faces[i].y));		
-		    		
-		Mat faceROI = frame_gray( faces[i] );
-    		std::vector<Rect> eyes;
-
-    		//-- In each face, detect eyes
-    		eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CV_HAAR_SCALE_IMAGE, Size(30, 30));
-
-    		for( size_t j = 0; j < eyes.size(); j++ )
-     		{
-       			Point center( faces[i].x + eyes[j].x + eyes[j].width*0.5, faces[i].y + eyes[j].y + eyes	[j].height*0.5 );
-       			int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
-       			circle( color, center, radius, Scalar( 255, 0, 0 ), 4, 8, 0 );
-     		}
-  	}
-  	//-- Show what you got
-  	//imshow( window_name, color );
-}
-void detectAndDisplay2( Mat ir )
-{
-  	std::vector<Rect> faces;
-
-  	//-- Detect faces
-  	face_cascade.detectMultiScale( ir, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30) );
-
-  	for( size_t i = 0; i < faces.size(); i++ )
-  	{
-    		Point center( faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5 );
-    		ellipse( ir, center, Size( faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
-
-    		Mat faceROI = ir( faces[i] );
-    		std::vector<Rect> eyes;
-
-    		//-- In each face, detect eyes
-    		eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CV_HAAR_SCALE_IMAGE, Size(30, 30));
-
-    		for( size_t j = 0; j < eyes.size(); j++ )
-     		{
-       			Point center( faces[i].x + eyes[j].x + eyes[j].width*0.5, faces[i].y + eyes[j].y + eyes	[j].height*0.5 );
-       			int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
-       			circle( ir, center, radius, Scalar( 255, 0, 0 ), 4, 8, 0 );
-     		}
-  	}
-}
 
 
